cc6e067dd49e701ef9d047f1749cde666561f70f
mdrag <m.dragay@yandex.ru>
2022-12-27 16:56:07 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        task1();
-        task2();
-    }
-    public static void task1 () {
         System.out.println("Задача 1");
-    }
-    public static void task2 () {
-        System.out.println("Задача 2");
+        byte turkmen = 127;
+        System.out.println("Значение переменной turkmen с типом byte равно " + turkmen);
+        short tajik = 32767;
+        System.out.println("Значение переменной tajik с типом short равно " + tajik);
+        int iranian = 2147483647;
+        System.out.println("Значение переменной iranaian с типом int равно " + iranian);
+        long uzbek = 9223372036854775807L;
+        System.out.println("Значение переменной uzbek с типом long равно " + uzbek);
+        float cossack = 3.400001f;
+        System.out.println("Значение переменной cossack с типом float равно " + cossack);
+        double jew = 1.7000000000000001;
+        System.out.println("Значение переменной jew с типом double равно " + jew);
     }
 }
